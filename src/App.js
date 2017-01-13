cccea60fd05d5db0f93440eb6d0131f2d0913f0e
Heasummn <heasummn@gmail.com>
2017-01-12 20:44:43 -0600

    Example of using React to define reuseable code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.sass';
 
-class App extends Component {
-  render() {
+// DO NOT USE, SIMPLE EXAMPLE TO ILLUSTRATE REACT
+const Pet = ({imageURL, name}) => {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Petreon!</h2>
+        <div>
+            <img src={imageURL} alt={name} />
+            <h2>{name}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    )
+};
+
+class MainApp extends Component {
+    render() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to Petreon!</h2>
+            </div>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Pet imageURL="http://orig15.deviantart.net/8661/f/2011/235/3/3/ballerina_by_deingel_dog_stock-d47kjyg.jpg"
+                 name="Phil"/>
+        </div>
     );
-  }
+    }
 }
 
-export default App;
+export default MainApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MainApp from './App';
 import './index.sass';
 
 ReactDOM.render(
-  <App />,
+  <MainApp />,
   document.getElementById('root')
 );
