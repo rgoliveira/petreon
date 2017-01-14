5ce6d6a9afc87f7b268d68a3d61f6d72f0e01597
lethargilistic <mikeoverby@outlook.com>
2017-01-14 16:34:45 -0500

    Linux development guide, start.php works on Linux

--- api/src/start.php
+++ api/src/start.php
@@ -14,17 +14,17 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . './settings.php';
+$settings = require __DIR__ . '/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . './dependencies.php';
+require __DIR__ . '/dependencies.php';
 
 // Register middleware
-require __DIR__ . './middleware.php';
+require __DIR__ . '/middleware.php';
 
 // Register routes
-require __DIR__ . './routes.php';
+require __DIR__ . '/routes.php';
 
 // Run app
 $app->run();
