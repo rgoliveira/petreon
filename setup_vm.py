416feba367857b92c3a22616cef4050a64119e61
fer22f <fer22f@gmail.com>
2017-01-20 13:08:10 -0200

    Make the setup_vm.py script a bit more intelligent. A bit.

--- setup_vm.py
+++ setup_vm.py
@@ -9,10 +9,22 @@ CURRENT_DIR = path.dirname(path.realpath(__file__))
 os.chdir(CURRENT_DIR)
 print('Setting up symlink for node_modules...')
 os.makedirs('/home/ubuntu/.sympm/petreon/node_modules', exist_ok=True)
-call(['ln', '-s', '/home/ubuntu/.sympm/petreon/node_modules', 'node_modules'])
+if path.islink('node_modules'):
+    print('Looks like you already got a symlink setup! Skipping...')
+elif path.isdir('node_modules'):
+    print('Hmm, interesting, you already got a non-symlinked folder setup')
+    print('I will just make sure it works if you made it on Windows...')
+    call(['npm', 'rebuild', 'node-sass'])
+else:
+    call(['ln', '-s', '/home/ubuntu/.sympm/petreon/node_modules', 'node_modules'])
+
 call(['npm', 'install'])
 
 os.chdir(path.join(CURRENT_DIR, 'api'))
-call(['php', 'composer.phar', 'install'])
+
+if path.isfile('composer.lock'):
+    call(['php', 'composer.phar', 'update'])
+else:
+    call(['php', 'composer.phar', 'install'])
 
 print('Setup done.')
