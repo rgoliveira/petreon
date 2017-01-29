11c7c9361732b88577d6b78f481a496627f2e3d5
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2017-01-29 12:34:21 +0000

    Basic test setup, as a showcase for #6

--- api/tests/test_env.py
+++ api/tests/test_env.py
@@ -1,2 +1,46 @@
-def test_travis_working():
-    assert True
+'''
+This file is intended to showcase a basic test setup with py.test.
+The setup for Flask testing is not complete and may require some more research.
+
+Everything here is subject to change, including the single-file structure
+(most likely).
+'''
+# TODO: Create conftest.py to make the fixtures for setting up the tests.
+
+
+class TestTravis:
+    "Meta tests for the CI environment, if needed."
+
+    def test_travis_working(self):
+        assert True
+
+
+class TestAPI:
+    "Tests for the basic API calls with mock data."
+
+    @classmethod
+    def setup_class(cls):
+        # TODO: Set up application testing, maybe pytest-flask.
+        # May require config refactoring
+        pass
+
+    def test_api_route(self):
+        # Not implemented
+        assert True
+
+
+class TestCRUD:
+    "Tests of the CRUD functionality with a proper DB."
+
+    @classmethod
+    def setup_class(cls):
+        # TODO: See above.
+        pass
+
+    def test_create(self):
+        assert True
+
+    @classmethod
+    def teardown_class(cls):
+        # TODO: Clean all the test data of the database.
+        pass
