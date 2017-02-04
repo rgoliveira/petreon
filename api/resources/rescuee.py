8abdd3758b82dcf769914506f98af1f8d2b016f2
Heasummn <hassamuddin20@gmail.com>
2017-02-03 22:13:55 -0600

    Added POST method to API for rescuee

--- api/resources/rescuee.py
+++ api/resources/rescuee.py
@@ -1,4 +1,5 @@
 from flask import jsonify, url_for
+from models import db
 from models.rescuee import Rescuee
 from flask_restful import reqparse, abort, Api, Resource
 from petreon_utils import to_dict
@@ -8,6 +9,12 @@ class RescueesAPI(Resource):
         return jsonify({"rescuees": to_dict(Rescuee.query.all())})
 
 class RescueeAPI(Resource):
+    
+    # Setup parser
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('name', type=str, default="Bigly", help="Name of the rescuee")
+    _parser.add_argument('kind', type=str, default="Doggo", help="Species of the rescuee")
+
     def get(self, rescuee_id):
         rescuee = Rescuee.query.filter_by(id=rescuee_id).first()
         if rescuee is None:
@@ -15,3 +22,15 @@ class RescueeAPI(Resource):
         else:
             return jsonify({"rescuee": to_dict(rescuee)})
 
+    def post(self, rescuee_id):
+        args = self._parser.parse_args()
+
+        if Rescuee.query.filter_by(id=rescuee_id).first() is not None:
+            # A rescuee with the same ID exists
+            # TODO: Maybe make a new unique id?
+            abort(400, message="Rescuee {} already exists".format(rescuee_id))
+
+        rescuee = Rescuee(id=rescuee_id, name=args["name"], kind=args["kind"])
+        db.session.add(rescuee)
+        db.session.commit()
+        return jsonify({"rescuee": to_dict(rescuee)})
