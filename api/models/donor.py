443f58e691ef98c32ed90c21ad474d0c9f1bcef1
Mike Overby <MikeOverby@outlook.com>
2017-02-09 23:15:57 -0500

    "Removing migration-ruining comma"
    This comma broke the automatic Alembic build, so there is no donor name in
    the table. We need to make a new migration.

--- api/models/donor.py
+++ api/models/donor.py
@@ -7,7 +7,7 @@ class Donor(db.Model):
     __tablename__ = 'donor'
 
     uuid        = db.Column(GUID, primary_key=True, default=uuid.uuid4)
-    name        = db.Column(db.String(255), nullable=False),
+    name        = db.Column(db.String(255), nullable=False)
     email       = db.Column(EmailType)
     verified    = db.Column(db.Boolean, nullable=False, default=False)
     donations   = db.relationship("Donation", backref="donor")
