d1bf2f5de60473590a9aca5bccee78bceec04aef
Mike Overby <MikeOverby@outlook.com>
2017-02-15 22:02:36 -0500

    Use the rescuee_id name instead of database uuid

--- api/resources/campaign.py
+++ api/resources/campaign.py
@@ -1,8 +1,12 @@
 from flask import jsonify, url_for
 from models import db
 from models.campaign import Campaign
+from models.rescuee import Rescuee
 from flask_restful import reqparse, abort, Api, Resource
 from petreon_utils import to_dict
+
+def get_rescuee_uuid(rescuee_id):
+    return Rescuee.query.filter_by(id=rescuee_id).first().uuid
 
 class CampaignAPI(Resource):
 
@@ -13,7 +17,9 @@ class CampaignAPI(Resource):
 
 
     def get(self, rescuee_id, campaign_type):
-        campaign = Campaign.query.filter_by(rescuee_uuid=rescuee_id,type=campaign_type).first()
+        rescuee_uuid = get_rescuee_uuid(rescuee_id)
+
+        campaign = Campaign.query.filter_by(rescuee_uuid=rescuee_uuid,type=campaign_type).first()
         if campaign is None:
             abort(404, message="Rescuee {} has no {} campaign!".format(rescuee_id, campaign_type))
 
@@ -21,13 +27,15 @@ class CampaignAPI(Resource):
 
 
     def post(self, rescuee_id, campaign_type):
+        rescuee_uuid = get_rescuee_uuid(rescuee_id)
+
         args = self._parser.parse_args()
 
-        if Campaign.query.filter_by(rescuee_uuid=rescuee_id, type=campaign_type).first() is not None:
+        if Campaign.query.filter_by(rescuee_uuid=rescuee_uuid, type=campaign_type).first() is not None:
             # TODO: Warn the user properly? Maybe make a unique name?
             abort(409, message="Rescuee {} already has a {} campaign!".format(campaign_type))
 
-        campaign = Campaign(rescuee_uuid=rescuee_id, type=campaign_type, current_amount=0, goal=args['goal'])
+        campaign = Campaign(rescuee_uuid=rescuee_uuid, type=campaign_type, current_amount=0, goal=args['goal'])
 
         db.session.add(campaign)
         db.session.commit()
@@ -36,7 +44,9 @@ class CampaignAPI(Resource):
 
 
     def delete(self, rescuee_id, campaign_type):
-        campaign = Campaign.query.filter_by(rescuee_uuid=rescuee_id, type=campaign_type).first()
+        rescuee_uuid = get_rescuee_uuid(rescuee_id)
+
+        campaign = Campaign.query.filter_by(rescuee_uuid=rescuee_uuid, type=campaign_type).first()
         if campaign is None:
             abort(404, message="Rescuee {} has no {} campaign!".format(rescuee_id, campaign_type))
 
@@ -51,7 +61,9 @@ class CampaignsAPI(Resource):
         '''
 	Rescuee to campaigns is a one-to-many relationship. Returns all the campaigns in JSON.
 	'''
-        campaigns = Campaign.query.filter_by(rescuee_uuid=rescuee_id).all()
+        rescuee_uuid = get_rescuee_uuid(rescuee_id)
+	
+        campaigns = Campaign.query.filter_by(rescuee_uuid=rescuee_uuid).all()
         if not campaigns:
             abort(404, message="Rescuee {} has no campaigns!".format(rescuee_id))
         campaigns = [to_dict(campaign) for campaign in campaigns]
